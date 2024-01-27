b55f33b292c52753676f58c4f63f13ff83b634d6
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-27 16:33:47 -0300

    feat: Instala os componentes na pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
+import { Hero } from "@/components/offTheHome/hero";
+import { About } from "@/components/offTheHome/about";
+import { Schedule } from "@/components/offTheHome/schedule";
+import { Contact } from "@/components/offTheHome/contact";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <>
+      <Head>
+        <title>Energia Holística</title>
+        <meta
+          name="description"
+          content="Sistema web para terapeutas com agendamento de consultas"
+        />
+      </Head>
+      <main>
+        <h1>HOME</h1>
+        <Hero />
+        <About />
+        <Schedule />
+        <Contact />
+      </main>
+    </>
   );
 }
