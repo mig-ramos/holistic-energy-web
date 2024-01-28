0a7ac69fbca25e46dc59447017b6085f65f0857c
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-28 00:02:32 -0300

    feat: Modifica componente Hero

--- src/components/offTheHome/hero.tsx
+++ src/components/offTheHome/hero.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import slide from "/public/images/praia.jpg";
+
 export function Hero() {
-    return <div>HERO</div>
-  }
+  return (
+    <section
+      className={`flex mx-auto my-auto rounded-3xl justify-center bg-zinc-100 mt-4`}
+    >
+      <Image src={slide} alt="Slides" className="rounded-3xl w-full" />
+    </section>
+  );
+}
