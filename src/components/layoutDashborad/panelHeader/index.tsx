c1da2116b74acfe6630d5c14a81fad1c4d467ddc
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-30 07:24:32 -0300

    feat: Altera estilos das paginas 404 e 500

--- src/components/layoutDashborad/panelHeader/index.tsx
+++ src/components/layoutDashborad/panelHeader/index.tsx
@@ -6,7 +6,7 @@ export function PanelHeader() {
   const { signOut } = useContext(AuthContext);
 
   return (
-    <div className="w-full flex items-center h-10 bg-red-500 rounded-lg text-white font-medium gap-4 px-4 mb-4">
+    <div className="w-full flex items-center h-10 bg-green-600 rounded-lg text-white font-medium gap-4 px-4 mb-4">
       <button className="ml-auto" onClick={signOut}>
         {" "}
         Sair da conta{" "}
